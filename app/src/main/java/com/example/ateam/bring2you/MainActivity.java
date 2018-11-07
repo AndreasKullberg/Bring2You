57f1a70ffb97f0ce28a495f640961802a190e6c3
andreaskullberg <andreas.t:kullberg@hotmail.com>
2018-11-07 10:20:31 +0100

    aKullbergs first

--- app/src/main/java/com/example/ateam/bring2you/MainActivity.java
+++ app/src/main/java/com/example/ateam/bring2you/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private int nummer = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
