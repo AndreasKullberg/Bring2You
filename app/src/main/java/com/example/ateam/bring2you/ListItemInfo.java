80eb467a89255574d2a8ea52d6c3cb427222552b
andreaskullberg <andreas.t:kullberg@hotmail.com>
2018-11-08 10:28:17 +0100

    Classes for lists

---
+++ app/src/main/java/com/example/ateam/bring2you/ListItemInfo.java
@@ -0,0 +1,33 @@
+package com.example.ateam.bring2you;
+
+public class ListItemInfo {
+    private String adress;
+    private String name;
+    private String postalCode;
+    private String senderId;
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public ListItemInfo(String adress, String name, String postalCode, String senderId) {
+        this.adress = adress;
+        this.name = name;
+        this.postalCode = postalCode;
+        this.senderId = senderId;
+    }
+
+
+}
