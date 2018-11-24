0d396fcccfeb5c6a0a4a58cd9be855cbc541cc37
alibaker <bib5_XED>
2018-11-24 16:16:53 +0100

    User preferences (language and theme) implemented EDIT: Removed unnecessary code

--- app/src/main/java/com/example/ateam/bring2you/MainActivity.java
+++ app/src/main/java/com/example/ateam/bring2you/MainActivity.java
@@ -39,12 +39,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Toolbar set up*/
+/*        *//* Toolbar set up*//*
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
         getSupportActionBar().setTitle("Order");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
 
 
         firebaseAuth = FirebaseAuth.getInstance();
