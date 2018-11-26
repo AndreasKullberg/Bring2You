9005256b50f9e08f842479da09b5e7b290115ad4
andreasblomqvist <adde-16@hotmail.com>
2018-11-26 12:21:13 +0100

    Fixed errors

--- app/src/main/java/com/example/ateam/bring2you/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/ateam/bring2you/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.ateam.bring2you;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -25,34 +26,32 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<ListItemViewHolder
     @Override
     public ListItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.list_item,viewGroup,false);
+                .inflate(R.layout.list_item, viewGroup, false);
         return new ListItemViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListItemViewHolder listItemViewHolder, int index) {
-    ListItemInfo item = listItems.get(index);
-    listItemViewHolder.setData(item);
-    listItemViewHolder.constraintLayout.setOnClickListener(view ->{
-        AppCompatActivity activity = (AppCompatActivity) view.getContext();
-    listItemViewHolder.constraintLayout.setOnClickListener(v ->{
-        AppCompatActivity activity = (AppCompatActivity) v.getContext();
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("Item",item);
-        Fragment signFragment = new SignFragment();
-        signFragment.setArguments(bundle);
-        activity.getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, signFragment)
-                .addToBackStack(null).commit();
+        ListItemInfo item = listItems.get(index);
+        listItemViewHolder.setData(item);
+        listItemViewHolder.constraintLayout.setOnClickListener(view -> {
+            AppCompatActivity activity = (AppCompatActivity) view.getContext();
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("Item", item);
+            Fragment signFragment = new SignFragment();
+            signFragment.setArguments(bundle);
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, signFragment)
+                    .addToBackStack(null).commit();
         });
 
-    listItemViewHolder.openMap.setOnClickListener(view -> {
-        // show marker google maps intent
+        listItemViewHolder.openMap.setOnClickListener(view -> {
+            // show marker google maps intent
 
 
-        Intent i = new Intent(view.getContext(), MapsActivity.class);
-        i.putExtra("mapKey", item);
-        view.getContext().startActivity(i);
-    });
+            Intent i = new Intent(view.getContext(), MapsActivity.class);
+            i.putExtra("mapKey", item);
+            view.getContext().startActivity(i);
+        });
     }
 
     @Override
@@ -60,13 +59,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<ListItemViewHolder
         return listItems.size();
     }
 
-    public void addItem(ListItemInfo info){
+    public void addItem(ListItemInfo info) {
         listItems.add(info);
-        this.notifyItemInserted(listItems.size()-1);
+        this.notifyItemInserted(listItems.size() - 1);
     }
 
-    private void removeItem(int index){
-        if( index >= 0 && index < listItems.size()) {
+    private void removeItem(int index) {
+        if (index >= 0 && index < listItems.size()) {
             listItems.remove(index);
             this.notifyItemRemoved(index);
         }
@@ -74,14 +73,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<ListItemViewHolder
 
     public void removeItem(String id) {
         for (int i = 0; i < listItems.size(); i++) {
-            if( listItems.get(i).getId().equals(id) ) {
+            if (listItems.get(i).getId().equals(id)) {
                 removeItem(i);
                 return;
             }
         }
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 }
