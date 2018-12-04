a9f55d3b500fed658ac2959fb993f53f659b32c4
fluzzyy <gunte95@hotmail.com>
2018-12-04 09:50:18 +0100

    removed unused imports

--- app/src/main/java/se/iths/ateam/bring2you/Activities/LoginActivity.java
+++ app/src/main/java/se/iths/ateam/bring2you/Activities/LoginActivity.java
@@ -2,8 +2,6 @@ package se.iths.ateam.bring2you.Activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -112,7 +110,6 @@ public class LoginActivity extends AppCompatActivity {
                                     toastMessage("Failure login in..");
                                 }
                             });
-
             });
 
         }
